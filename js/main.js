bbe0aded612ec62a1799c4cb07cf687f14891b22
frank2nyy <frank2nyyy@gmail.com>
2019-05-11 19:23:00 +0300

    uploaded the project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+
+
+ function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
