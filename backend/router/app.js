1bfa0a156c9893da49c37e46811d677be4f998af
frank2nyy <frank2nyyy@gmail.com>
2019-05-31 15:03:20 +0300

    configure the postgre SQL

--- backend/router/app.js
+++ backend/router/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 // const router = express.Router();
 const app = express();
 const data = [
-    {id: 1, order: 1,from: "Mbezi", to:"kinondoni", cost:5000, createdOn: new Date()},
-    {id: 2, order: 2,from: "Tegeta", to:"AIR Port", cost:15000, createdOn: new Date()},
-    {id: 3, order: 3,from: "mbagala", to:"AIR Port", cost:14000, createdOn: new Date()},
-
+  {id:1,driver: "Juma" ,vehicle:"lancer",from: "mwenge", to:"AIR Port", cost:14000, createdOn: new Date()},
+  {id:2,driver: "henry" ,vehicle:"subaru",from: "mbagala", to:"AIR Port", cost:14000, createdOn: new Date()},
+  {id:3,driver: "Jackson" ,vehicle:"collora",from: "sinza", to:"AIR Port", cost:14000, createdOn: new Date()},
+  {id:4,driver: "Adam" ,vehicle:"collora",from: "mbagala", to:"AIR Port", cost:14000, createdOn: new Date()}
     ];
 //get all rides
 app.get('/', function (req, res) {
@@ -24,50 +24,52 @@ app.get('/:id', function (req, res) {
         res.sendStatus(404);
     }
 });
-//edit ride
-app.put('/:id', function (req, res) {
-  let found = data.find(function (ride) {
-    return ride.id === parseInt(req.params.id);
-  });
 
-  if (found) {
-    let updated = {
-      id: found.id,
-      from: req.body.from,
-      to:req.body.to,
-      cost: req.body.cost,
-    };
+//offer ride
+// //edit ride
+// app.put('/:id', function (req, res) {
+//   let found = data.find(function (ride) {
+//     return ride.id === parseInt(req.params.id);
+//   });
 
-    let targetIndex = data.indexOf(found);
+//   if (found) {
+//     let updated = {
+//       id: found.id,
+//       from: req.body.from,
+//       to:req.body.to,
+//       cost: req.body.cost,
+//     };
 
-    data.splice(targetIndex, 1, updated);
+//     let targetIndex = data.indexOf(found);
 
-    res.sendStatus(204);
-  } else {
-    res.sendStatus(404);
-  }
-});
-//delete rides
-app.delete('/:id', function (req, res) {
-  let found = data.find(function (ride) {
-    return ride.id === parseInt(req.params.id);
-  });
+//     data.splice(targetIndex, 1, updated);
 
-  if (found) {
-    let targetIndex = data.indexOf(found);
+//     res.sendStatus(204);
+//   } else {
+//     res.sendStatus(404);
+//   }
+// });
+// //delete rides
+// app.delete('/:id', function (req, res) {
+//   let found = data.find(function (ride) {
+//     return ride.id === parseInt(req.params.id);
+//   });
 
-    data.splice(targetIndex, 1);
-  }
+//   if (found) {
+//     let targetIndex = data.indexOf(found);
 
-  res.sendStatus(204);
-});
+//     data.splice(targetIndex, 1);
+//   }
 
-//make request to join ride
-app.post('/offer/ride',(req,res)=>{
+//   res.sendStatus(204);
+// });
 
-})
-app.post('/v1/join/:id',(req,res)=>{
+// //make request to join ride
+// app.post('/offer/ride',(req,res)=>{
 
-})
+// })
+// app.post('/v1/join/:id',(req,res)=>{
+
+// })
 
 module.exports = app;
